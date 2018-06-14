ef7de5601ad81c20abf01afcb0d7c6be0f8ce6d4
Disa0808 <levshukdenis@mail.ru>
2018-06-14 22:44:24 +0300

    Add user login and register form (without backend logic)

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const path = require('path');
 
 const app = express();
+
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
 
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev')
     .then(() => console.log('MongoDB Connected.....'))
     .catch(err => console.log(err));
-
-// Load Idea Model
-require('./models/idea');
-const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -23,6 +26,29 @@ app.set('view engine', 'handlebars');
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Express session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');   
+    next();
+});
 
 // Index Route
 app.get('/', (req, res) => {
@@ -37,61 +63,8 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({date: 'desc'})
-        .then(ideas => {
-            res.render('ideas/index', {ideas: ideas});
-        });
-});
-
-
-// Add idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-});
-
-// Edit idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    }).then(idea => {
-        res.render('ideas/edit', {
-            idea: idea
-        })
-    });
-    
-});
-
-// Process form
-app.post('/ideas', (req, res) => {
-    let errors = [];
-
-    if(!req.body.title){
-        errors.push({text: 'Please add a title'});
-    }
-    if(!req.body.details){
-        errors.push({text: 'Please add some details'});
-    }   
-    if(errors.length > 0){
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                res.redirect('/ideas');
-            });
-    }
-});
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 
